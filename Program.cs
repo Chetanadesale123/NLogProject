e899f989bf48f648be1fdc604442f50d41e35b8f
59114_Chetana <deslechetna1999@gmail.com>
2022-04-21 11:39:30 +0530

    [Chetana] added:NLogProject

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using System;
+namespace NLogProject
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {   NLogProject nLog = new NLogProject();
+            AddNumbers numbers = new AddNumbers();
+            numbers.Sum(10, 40);
+        }
+    }  
+}
